d95d505c2526983d5f135737e9da922b06f49108
aniketsawant50 <aniketsawant016@gmail.com>
2025-07-28 16:28:46 +0530

    Create lastnode.java

---
+++ lastnode.java
@@ -0,0 +1,53 @@
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+
+
+Answers:
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode temp = head;
+        int size = 0;
+        while(temp != null){
+            temp=temp.next;
+            size++;
+        }
+        if(n==size ){
+            head = head.next;
+            return head;
+        }
+        ListNode dummy = head;
+
+        for(int i = 1 ; i < size-n; i++){
+            dummy = dummy.next;
+        }
+        dummy.next = dummy.next.next;
+
+        return head ;
+    }
+}
